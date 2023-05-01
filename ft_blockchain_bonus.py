6558dc48634b3b15113cf521d263391434f11463
José Antonio <104014631+JoseRosique@users.noreply.github.com>
2023-05-01 21:10:31 +0200

    done

--- ft_blockchain_bonus.py
+++ ft_blockchain_bonus.py
@@ -1,92 +1,104 @@
-from flask import Flask, jsonify, request
 import hashlib
-import datetime
-import random
 import json
+import requests
+from time import time
+from urllib.parse import urlparse
+import random
+import string
+import hashlib
+from flask import Flask, jsonify, request
+from uuid import uuid4
 
-class Validator:
-    def __init__(self, name, address, balance, stake):
+class Account:
+    def __init__(self, name, address, balance):
         self.name = name
         self.address = address
         self.balance = balance
-        self.stake = stake
-        
-    def __repr__(self):
-        return f"{self.name} ({self.address})"
-    
-    def to_dict(self):
-        return {
-            "name": self.name,
-            "address": self.address,
-            "balance": self.balance,
-            "stake": self.stake
-        }
 
-class Block:
-    def __init__(self, transactions, prev_block_hash, validator):
-        self.transactions = transactions
-        self.timestamp = datetime.datetime.now()
-        self.prev_block_hash = prev_block_hash
-        self.validator = validator
-        self.hash = self.calculate_hash()
-        
-    def calculate_hash(self):
-        block_contents = str(self.timestamp) + str(self.prev_block_hash) + str(self.transactions) + str(self.validator.address)
-        return hashlib.sha256(block_contents.encode()).hexdigest()
-        
-    def __repr__(self):
-        return f"Block({self.hash}, {self.prev_block_hash}, {self.transactions}, {self.validator})"
-    
-    def to_dict(self):
-        return {
-            "timestamp": self.timestamp.strftime('%Y-%m-%d %H:%M:%S'),
-            "prev_block_hash": self.prev_block_hash,
-            "transactions": self.transactions,
-            "validator": self.validator.to_dict(),
-            "hash": self.hash
-        }
-
-class Blockchain:
-    def __init__(self, genesis_block, num_validators):
-        self.chain = [genesis_block]
+class Blockchain(object):
+    def __init__(self, num_validators):
+        self.chain = []
+        self.nodes = set()
+        self.transactions = []
         self.validators = self.generate_validators(num_validators)
-        
-    def add_block(self, transactions):
-        prev_block = self.chain[-1]
-        validator = self.select_validator()
-        new_block = Block(transactions, prev_block.hash, validator)
-        self.chain.append(new_block)
-        return new_block
+
+        self.new_block(previous_hash=1)
     
+    def new_block(self, previous_hash=None):
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.transactions,
+            'previous_hash': previous_hash or self.hash(self.chain[-1]),
+        }
+        self.chain.append(block)
+        self.transactions = []
+        return block
+    
+    def new_transaction(self, sender, recipient, amount, balance):
+        self.transactions.append({
+            'sender': sender,
+            'recipient': recipient,
+            'amount': amount,
+            'balance': balance,
+        })
+        return self.last_block['index'] + 1
+    
+    @staticmethod
+    def hash(block):
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
+    
+    @property
+    def last_block(self):
+        return self.chain[-1]
+    
+    def generate_random_address(self):
+        letters = string.ascii_letters + string.digits
+        random_string = ''.join(random.choice(letters) for i in range(32))
+        return random_string
+
     def generate_validators(self, num_validators):
         validators = []
         for i in range(num_validators):
-            name = f"Validator{i}"
-            address = f"address{i}"
-            balance = 1000
-            stake = random.randint(1, 100)
-            validators.append(Validator(name, address, balance, stake))
+            name = f"Account{i}"
+            address = self.generate_random_address()+str(i)
+            balance = random.randint(1, 100)
+            validators.append(Account(name, address, balance))
         return validators
-    
-    def select_validator(self):
-        total_stake = sum([v.stake for v in self.validators])
-        target = random.uniform(0, total_stake)
-        cum_stake = 0
-        for validator in self.validators:
-            cum_stake += validator.stake
-            if cum_stake > target:
-                return validator
-    
-    def validate(self):
-        for i in range(1, len(self.chain)):
-            curr_block = self.chain[i]
-            prev_block = self.chain[i-1]
-            if curr_block.prev_block_hash != prev_block.hash:
-                return False
-            if curr_block.hash != curr_block.calculate_hash():
-                return False
-        return True
 
+    def proof_of_stake(self):
+        total_stake = sum([v.balance for v in self.validators])
+        rand = random.uniform(0, total_stake)
+        selected_validator = None
+        for validator in self.validators:
+            if validator.balance == 0:
+                validator.balance = 1
+            if rand < validator.balance:
+                selected_validator = validator
+                validator.balance -= 1
+                break
+            rand -= validator.balance
+        selected_validator.balance += 42
+        return selected_validator
+
+    def register_node(self, address):
+        self.nodes.add((urlparse(address)).netloc)
+    
+    def valid_chain(self, chain):
+        last_block = chain[0]
+        current_index = 1
+
+        while current_index < len(chain):
+            block = chain[current_index]
+            print(last_block)
+            print(block)
+            if block['previous hash'] != self.hash(last_block):
+                return False
+            last_block = block
+            current_index += 1
+        return True
+    
     def get_balance(self, address):
         balance = 0
         for block in self.chain:
@@ -94,79 +106,97 @@ class Blockchain:
                 balance += 1
         return balance
     
-    def __repr__(self):
-        return f"Blockchain({self.chain})"
-    
-    def to_dict(self):
-        return {
-            "chain": [block.to_dict() for block in self.chain],
-            "validators": [validator.to_dict() for validator in self.validators]
-        }
-
-# Create blockchain
-genesis_block = Block([], "0", Validator("Alice", "address1", 100, 50))
-blockchain = Blockchain(genesis_block, 5)
-
-# Create Flask app
+    def solve_conflicts(self):
+        neighbours = self.nodes
+        new_chain = None
+        max_length = len(self.chain)
+        for node in neighbours:
+            response = requests.get(f'http://{node}/chain')
+            if response.status_code == 200:
+                length = response.json()['length']
+                chain = response.json()['chain']
+                if length > max_length and self.valid_chain(chain):
+                    new_chain = chain
+                    max_length = length
+        if new_chain is not None:
+            self.chain = new_chain
+            return True
+        else:
+            return False
+        
 app = Flask(__name__)
 
-# Custom JSON encoder class to serialize Block and Validator objects
-class CustomJSONEncoder(json.JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, Block):
-            return obj.to_dict()
-        
-        if isinstance(obj, Validator):
-            return obj.to_dict()
-        
-        return super().default(obj)
-
-# Set the JSON encoder for the app
-app.json_encoder = CustomJSONEncoder
-
-# POST request to add transaction
 @app.route('/transactions/new', methods=['POST'])
 def new_transaction():
     values = request.get_json()
-    required = ['sender', 'recipient', 'amount']
-    if not all(k in values for k in required):
+    if not all(item in values for item in ['sender', 'recipient', 'amount', 'balance']):
         return 'Missing values', 400
-        
-    transactions = [f"Transaction {blockchain.chain[-1]}-1", f"Transaction {blockchain.chain[-1]}-2"]
-    block = blockchain.add_block(transactions)
-    response = {'message': f'Transaction will be added to Block {block.hash}'}
+    index = blockchain.new_transaction(values['sender'], values['recipient'], values['amount'], values['balance'])
+    response = {'message': f'Transaction will be added to Block {index}'}
     return jsonify(response), 201
 
-# GET request to mine a new block
 @app.route('/mine', methods=['GET'])
 def mine():
-    transactions = [f"Transaction {blockchain.chain[-1]}-1", f"Transaction {blockchain.chain[-1]}-2"]
-    block = blockchain.add_block(transactions)
+    last_block = blockchain.last_block
+    selected=(blockchain.proof_of_stake())
+    blockchain.new_transaction(
+        sender="0",
+        recipient=selected.address,
+        amount=42,
+        balance=selected.balance
+    )
+    previous_hash = blockchain.hash(last_block)
+    block = blockchain.new_block(previous_hash)
     response = {
         'message': "New Block Forged",
-        'index': len(blockchain.chain),
-        'transactions': block.transactions,
-        'prev_block_hash': block.prev_block_hash,
-        'validator': block.validator,
-        'hash': block.hash,
+        'index': block['index'],
+        'timestamp': block['timestamp'],
+        'transactions': block['transactions'],
+        'previous_hash': block['previous_hash']
     }
     return jsonify(response), 200
 
-# GET request to get the full blockchain
-@app.route('/chain', methods=['GET'])
+@app.route('/chain', methods = ['GET'])
 def full_chain():
     response = {
         'chain': blockchain.chain,
-        'length': len(blockchain.chain),
+        'length':len(blockchain.chain)
     }
     return jsonify(response), 200
 
+@app.route('/nodes/register', methods=['POST'])
+def register_nodes():
+    values = request.get_json()
+    nodes = values.get('nodes')
+    if nodes is None:
+        return "Error: Please supply a valid list of nodes", 400
+    for node in nodes:
+        blockchain.register_node(node)
+    response = {
+        'message': 'New nodes have been added',
+        'total_nodes': list(blockchain.nodes),
+    }
+    return jsonify(response), 201
 
-# Run the app
+@app.route('/nodes/resolve', methods=['GET'])
+def consensus():
+    replaced = blockchain.solve_conflicts()
+    if replaced:
+        response = {
+            'message': 'Our chain was replaced',
+            'new_chain': blockchain.chain
+        }
+    else:
+        response = {
+            'message': 'Our chain is still valid',
+            'new_chain': blockchain.chain
+        }
+    return jsonify(response), 200
+
 if __name__ == '__main__':
-    from argparse import ArgumentParser
-    parser = ArgumentParser()
-    parser.add_argument('-p', '--port', default=500, type=int, help='port to listen on')
-    args = parser.parse_args()
-    port = args.port
-    app.run(host='0.0.0.0', port=port)
+    print("Introduce Port:")
+    port_i = int(input())
+    print("\nIntroduce number of accounts:")
+    number_acc = int(input())
+    blockchain = Blockchain(number_acc)
+    app.run(host='0.0.0.0', port = port_i)
